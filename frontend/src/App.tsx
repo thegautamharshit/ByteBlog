e179c0959b93aab51aa0c3a6c939361105e555ab
Harshit Gautam <thegautamharshit@gmail.com>
2025-03-28 06:59:00 +0530

    Added Default Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import {Signup} from './pages/Signup';
 import {Signin} from './pages/Signin';
 import {Blog} from './pages/Blog';
@@ -11,6 +11,8 @@ function App(){
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element = {<Navigate to ="/signin" />} />
+
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
